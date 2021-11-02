a6cab3ae80584a9ac9696ad2610778f37ac32c02
worldoverride <89107673+worldoverride@users.noreply.github.com>
2021-11-02 23:35:07 +0100

    Update solve.py

--- 2021/KillerQueen/Deoxyencoded Nucleic Acid/solve.py
+++ 2021/KillerQueen/Deoxyencoded Nucleic Acid/solve.py
@@ -5,6 +5,6 @@ dna = {'A': '00', 'T': '01', 'G': '10', 'C': '11'}
 with open('dna1.txt') as f:
     s = f.read()
 
-    dec = ''.join([chr(int(dna[s[i]]+dna[s[i+1]]+dna[s[i+2]]+dna[s[i+3]], 2)) for i in range(0, len(s) - 4, 4)])
+    flag = ''.join([chr(int(dna[s[i]]+dna[s[i+1]]+dna[s[i+2]]+dna[s[i+3]], 2)) for i in range(0, len(s) - 4, 4)])
 
-    print(dec)
+    print(flag)
